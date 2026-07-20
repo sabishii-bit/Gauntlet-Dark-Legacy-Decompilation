d100b32c5a9870ecba1e73d0239f65c5eaed2382
sabishii-bit <cmsheets@pm.me>
2026-07-19 20:28:06 -0700

    MetroTRK support.c TU translated

--- configure.py
+++ configure.py
@@ -355,6 +355,7 @@ config.libs = [
             Object(Matching, "TRK_MINNOW_DOLPHIN/Portable/serpoll.c"),
             Object(Matching, "TRK_MINNOW_DOLPHIN/Portable/dispatch.c"),
             Object(Matching, "TRK_MINNOW_DOLPHIN/Portable/msghndlr.c"),
+            Object(Matching, "TRK_MINNOW_DOLPHIN/Portable/support.c"),
             Object(Matching, "TRK_MINNOW_DOLPHIN/Portable/usr_put.c"),
         ],
     },
