7513a29d0ae8f02dada36c0a76a9e20186cef49a
sabishii-bit <cmsheets@pm.me>
2026-07-25 19:44:19 -0700

    Add game/sys/ml_mem.c (ML_MEM allocator+file loader, NonMatching): 26 fns + 26 data mapped incl bulletproof_printf (48 callers)

--- configure.py
+++ configure.py
@@ -405,6 +405,7 @@ config.libs = [
             Object(NonMatching, "game/boss/boss.c", cflags=cflags_demo, mw_version="GC/1.2.5"),
             Object(NonMatching, "game/pb/pb_diag.c", cflags=cflags_demo, mw_version="GC/1.2.5"),
             Object(NonMatching, "game/audio/soundmgr.c", cflags=cflags_demo, mw_version="GC/1.2.5"),
+            Object(NonMatching, "game/sys/ml_mem.c", cflags=cflags_demo, mw_version="GC/1.2.5"),
         ],
     },
     {
