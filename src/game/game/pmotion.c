befc75aea54f811696b4af511727ae285f34a498
sabishii-bit <cmsheets@pm.me>
2026-08-28 15:47:52 -0700

    Improve PlayerMotion: f64 promotions at post-join hit recompute (fuzzy 83.89 -> 83.91)

--- src/game/game/pmotion.c
+++ src/game/game/pmotion.c
@@ -2242,10 +2242,10 @@ store_motion_state:
                         effectVelocity[1] = hit[1] - PF(p, 0x58, f32);
                         effectVelocity[2] = hit[2] - PF(p, 0x5C, f32);
                         NormalVector(effectVelocity);
-                        hit[0] = PF(p, 0x54, f32) + effectVelocity[0];
+                        hit[0] = (f32)((f64)effectVelocity[0] + PF(p, 0x54, f32));
                         hit[1] = (f32)(PF(p, 0x58, f32) +
                                       effectVelocity[1] + lbl_80347C28);
-                        hit[2] = PF(p, 0x5C, f32) + effectVelocity[2];
+                        hit[2] = (f32)((f64)effectVelocity[2] + PF(p, 0x5C, f32));
                         effect = StartFXSub(93, hit, effectFlags | 8, 0,
                                             lbl_80347CA8);
                         effectVelocity[0] *= bossDamage;
