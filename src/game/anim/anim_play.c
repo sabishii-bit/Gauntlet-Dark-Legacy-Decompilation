514d5d6051218a58dda7d1fa59ad1d476ed06510
sabishii-bit <cmsheets@pm.me>
2026-08-31 16:35:59 -0700

    Improve GetAnimAngXYZVal match (real 160 -> 156, f64 constant load order)

--- src/game/anim/anim_play.c
+++ src/game/anim/anim_play.c
@@ -533,9 +533,13 @@ s32 GetAnimAngXYZVal(f32 frame, AnimData* data, f32* pose, u32* keydata, s32 fla
         oscale[2] = pscale[2];
     }
     {
-        f64 cycle = lbl_80345800;
-        f64 lower = lbl_80345808;
-        f64 upper = lbl_803457F8;
+        f64 cycle;
+        f64 lower;
+        f64 upper;
+
+        lower = lbl_80345808;
+        cycle = lbl_80345800;
+        upper = lbl_803457F8;
 
         for (i = 0; i < 3; i++) {
         f64 v = pose[i];
