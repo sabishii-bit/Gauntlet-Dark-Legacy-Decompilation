dc70f0af288c362bca3bf8a2096fb7ed1f9b1448
sabishii-bit <cmsheets@pm.me>
2026-09-01 10:31:31 -0700

    Fix wrong-global read at camera_mode_level +0x248 (lbl_80344530 -> lbl_80344408)

--- src/game/world/camera.c
+++ src/game/world/camera.c
@@ -2666,7 +2666,7 @@ void camera_mode_level(s32 reset)
         camera_collide_step(0, lbl_8034601C);
         *(volatile f32*)&lbl_80344408 =
             *(volatile f32*)&lbl_80344530;
-        cam0->pyr[0] = *(volatile f32*)&lbl_80344530;
+        cam0->pyr[0] = *(volatile f32*)&lbl_80344408;
         cam0->pyr[1] = *(volatile f32*)&lbl_80344534;
     }
     calc_cam_pyr(0, 1);
