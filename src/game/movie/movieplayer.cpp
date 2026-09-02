f1fbfe6f94711b0984250909908a2278c6811e2b
sabishii-bit <cmsheets@pm.me>
2026-09-02 15:52:10 -0700

    Improve fn_800D8BCC match (real 164 -> 140, 87 -> 76 differing words, fuzzy 95.8605 -> 96.3488)

--- src/game/movie/movieplayer.cpp
+++ src/game/movie/movieplayer.cpp
@@ -816,6 +816,7 @@ u32 fn_800D8BCC(MovieDecodeState* state, int param_2, char* param_3, int param_4
         int row;
         int d8;
         int d2;
+        int y;
 
         if (*(int*)(param_5 + offsetof(MovieBitmapHeader, height)) < 0) {
             dir = -1;
@@ -828,7 +829,6 @@ u32 fn_800D8BCC(MovieDecodeState* state, int param_2, char* param_3, int param_4
             u8 bits;
             u8 nb;
             u8* bp;
-            int y;
             int w;
 
             w = state->width;
@@ -875,8 +875,6 @@ u32 fn_800D8BCC(MovieDecodeState* state, int param_2, char* param_3, int param_4
             }
             state->width = state->width / 2;
         } else {
-            int y;
-
             d8 = dir * 8;
             d2 = dir * 2;
             state->width <<= 1;
